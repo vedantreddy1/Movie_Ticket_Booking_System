e6ee9a597525e7b13b840690b3e8a281b06e8cc9
VedantR27 <vedant@lifecircle.in>
2026-01-15 15:47:40 +0530

    fix css path casing issue for vercel

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, {  useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "../Styles/Signup.css";
+import "../styles/Signup.css";
 import axios from "axios";
 
 const Signup = () => {
